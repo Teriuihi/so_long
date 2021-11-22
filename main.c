95f07a2b27dd59e211dcb122e27b008fe03dfc3c
Stijn Appunn <sappunn@student.codam.nl>
2021-11-22 21:51:31 +0100

    Moved getting file to it's own function

--- main.c
+++ main.c
@@ -2,21 +2,35 @@
 #include <fcntl.h>
 #include "header_rename.h"
 
-int	main(int len, char **args)
+t_list	*get_file(int len, char **args)
 {
-	int	fd;
+	int		fd;
+	t_list	*file;
 
 	if (len != 2)
 	{
 		write(1, "Error\n", 6);
-		return (0);
+		exit(0);
 	}
 	fd = open(args[1], O_RDONLY);
 	if (fd < 0)
 	{
 		write(1, "Error\n", 6);
-		return (0);
+		exit(0);
 	}
-	load_file(fd);
+	file = load_file(fd);
+	if (file == NULL)
+	{
+		write(1, "Error\n", 6);
+		exit(0);
+	}
+	return (file);
+}
+
+int	main(int len, char **args)
+{
+	t_list *file;
+
+	file = get_file(len, args);
 	return (0);
 }
